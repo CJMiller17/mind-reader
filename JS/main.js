da750cf9da08f1afe210a575fda97cc9bf8a0ed5
Cody <c.jmiller17@yahoo.com>
2024-04-03 17:58:15 -0400

    large changes to variables and JS script

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,76 @@
-/* Grab the image
-Grab the h5
+let header = document.querySelector("h5")
+let nextBtn = document.querySelector("button")
+let paragraph = document.querySelector("p")
+let resetBtn = document.getElementById("resetBtn")
 Grab the p
 Grab the previous button
 Grab the next button
 Grab the reset
 
+viewState = 0;
 
-*/
+EventlISnte. click NEXT i++
+Evenliste   Fn => viewState = 0
+
+Fn 
+Bracket notation
+
+let stateArray = [ ]
+    State 0: {
+        image: IMG/srfnxbjdnb.png
+        header:
+        p:
+        prev:
+        next: null;
+        reset: 
+    }
+    State 3: {
+        image: IMG/srfnxbjdnb.png
+        header:
+        p:
+        prev:
+        next: null;
+        reset: 
+    }
+    State 4: {
+        image: IMG/srfnxbjdnb.png
+        header:
+        p:
+        prev:
+        next: null;
+        reset: 
+    }
+    State 5: {
+        image: IMG/srfnxbjdnb.png
+        header:
+        p:
+        prev:
+        next: null;
+        reset: 
+    }
+    State 6: {
+        image: IMG/srfnxbjdnb.png
+        header:
+        p:
+        prev:
+        next: null;
+        reset: 
+    }
+    State 7: {
+        image: IMG/srfnxbjdnb.png
+        header:
+        p:
+        prev:
+        next: null;
+        reset: 
+    }
+
+let myArray = [
+  {id: 0, name: "Jhon"},
+  {id: 1, name: "Sara"},
+  {id: 2, name: "Domnic"},
+  {id: 3, name: "Bravo"}
+],
+
+//Update object's name property.
+myArray[objIndex].name = "Laila"
